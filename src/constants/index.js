1dd1953d4ffed1694a59a9dae3acd1950d16523e
SergeyVovchenko <serega.vovchenko@gmail.com>
2024-10-09 17:28:14 +0200

    add reset password

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,26 @@
+import path from 'node:path';
+
 export const SORT_ORDER = {
   ASC: 'asc',
   DESC: 'desc',
 };
 
+export const SMTP = {
+  SMTP_HOST: 'SMTP_HOST',
+  SMTP_PORT: 'SMTP_PORT',
+  SMTP_USER: 'SMTP_USER',
+  SMTP_PASSWORD: 'SMTP_PASSWORD',
+  SMTP_FROM: 'SMTP_FROM',
+};
+
+export const JWT = {
+  JWT_SECRET: 'JWT_SECRET',
+};
+
 export const FIFTEEN_MINUTES = 15 * 60 * 1000;
 export const THIRTEEN_DAYS = 30 * 24 * 60 * 60 * 1000;
+export const JWT_LIVE_TIME = 5 * 60;
+
+export const APP_DOMAIN = 'APP_DOMAIN';
+
+export const TEMPLATES_DIR = path.join(process.cwd(), 'src', 'templates');
--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,10 +1,12 @@
 import { THIRTEEN_DAYS } from '../constants/index.js';
-// import { SessionsCollection } from '../db/models/session.js';
+
 import {
   loginUser,
   logoutUser,
   refreshUserSession,
   registerUser,
+  requestResetToken,
+  resetPassword,
 } from '../services/auth.js';
 
 export const authUserController = async (req, res, next) => {
@@ -76,4 +78,23 @@ export const logoutUserController = async (req, res, next) => {
   res.clearCookie('refreshToken');
 
   res.status(204).send();
+};
+
+export const requestResetEmailController = async (req, res, next) => {
+  await requestResetToken(req.body.email);
+
+  res.status(200).json({
+    message: 'Reset password email was successfully sent!',
+    status: 200,
+    data: {},
+  });
+};
+
+export const resetPasswordController = async (req, res, next) => {
+  await resetPassword(req.body);
+  res.status(200).json({
+    message: 'Password was successfully reset!',
+    status: 200,
+    data: {},
+  });
 };
